e640b233cce208303cc54e1ae481d52b471fce2f
OneGuabaya <viloriajep@gmail.com>
2022-09-18 18:25:37 -0400

    feat(make): js documents with tasks added

---
+++ charlie/hack1.js
@@ -0,0 +1,9 @@
+
+let numbers = [100,200,300,400,500,600,700]
+let quantity = numbers.length - 1
+
+for (times = 0; times <= quantity; times++) {
+    numbers.pop()
+    console.log(numbers)
+}
+
---
+++ charlie/hack9.js
@@ -0,0 +1,6 @@
+
+let numbers = [100,200,300,400,500,600,700]
+let alias = "charlie"
+numbers.unshift(alias)
+
+console.log(numbers)
